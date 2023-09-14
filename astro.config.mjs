cb23312b9722b917638c3b34581f671579b78d8d
dali khaled <medalikhaled331@gmail.com>
2023-09-14 15:55:29 +0100

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService  } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
